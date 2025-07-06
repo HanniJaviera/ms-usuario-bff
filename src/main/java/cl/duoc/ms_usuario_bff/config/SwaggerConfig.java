dcaba437f497b2cf91a81bf040b41e0ef3db9987
HanniJaviera <javiera.arc@hotmail.com>
2025-07-06 12:48:29 -0400

    swagger config

---
+++ src/main/java/cl/duoc/ms_usuario_bff/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package cl.duoc.ms_usuario_bff.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiInfo(){
+        return new OpenAPI()
+                   .info(new Info().title("Documentación Usuario BFF")
+                                    .description("A continuación puedes ver los métodos a utilizar")
+                                    .version("1.0.0"));
+    }
+
+}
